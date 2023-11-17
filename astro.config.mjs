52662920937019c4b80d809277dc47ed3393e7c0
Michael H <56214343+RiskyMH@users.noreply.github.com>
2023-11-17 16:15:47 +1100

    More better!
    scroll less weird :)
    better mobile :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
+// import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    // react()
+  ],
   experimental: {
-    contentCollectionCache: true
-  }
+    contentCollectionCache: true,
+  },
 });
