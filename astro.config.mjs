7b8eebf265b9d35946582ec1719d808261036c45
RiskyMH <56214343+RiskyMH@users.noreply.github.com>
2025-03-04 18:08:27 +1100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 // import tailwind from "@astrojs/tailwind";
 import tailwindcss from '@tailwindcss/vite';
-import tailwindcssCleaner from "./.vite/tailwind-cleaner";
 import min from 'astro-min';
 
 
@@ -34,7 +33,6 @@ export default defineConfig({
   vite: {
     plugins: [
       tailwindcss(),
-      tailwindcssCleaner(),
     ],
     build: {
       assetsInlineLimit: 100000,
